7cc2c77866e7e85855c17d9b5394b7ca2306f003
iqranoor738 <ahmedsultanrs58@gmail.com>
2026-07-05 19:27:02 +0500

    counter app

---
+++ app.js
@@ -0,0 +1,21 @@
+
+let count = 0;
+
+function updateDisplay() {
+  document.getElementById("counter").innerText = count;
+}
+
+function increase() {
+  count++;
+  updateDisplay();
+}
+
+function decrease() {
+  count--;
+  updateDisplay();
+}
+
+function reset() {
+  count = 0;
+  updateDisplay();
+}
